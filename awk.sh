6753f879106dc2f99fbc523afdbc3ea4bc80dbd9
fyan0706 <62992249+fyan0706@users.noreply.github.com>
2020-04-09 16:10:04 +0800

    Create awk.sh

---
+++ awk.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/bash
+
+# 打印文件的 第2 到 最后一列  用tab隔开
+awk '{for(i=2;i<=NF;++i) printf $i "\t";printf "\n"}' file.txt
+# 打印文件的 第2 到 最后一列  用空格隔开
+awk '{for(i=2;i<=NF;++i) printf $i " ";printf "\n"}' file.txt
