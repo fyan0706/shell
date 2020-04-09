21a40560d53e5b77ea5d9867c8d97b02d476724f
fyan0706 <62992249+fyan0706@users.noreply.github.com>
2020-04-09 16:13:03 +0800

    有用的awk用法

--- awk.sh
+++ awk.sh
@@ -2,5 +2,5 @@
 
 # 打印文件的 第2 到 最后一列  用tab隔开
 awk '{for(i=2;i<=NF;++i) printf $i "\t";printf "\n"}' file.txt
-# 打印文件的 第2 到 最后一列  用空格隔开
+# 打印文件的 第2 到 最后一列  用空格隔开 
 awk '{for(i=2;i<=NF;++i) printf $i " ";printf "\n"}' file.txt
